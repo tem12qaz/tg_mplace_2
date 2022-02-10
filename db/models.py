a0b50fa5dc202240851a01602075c4499025f621
Matvey <tem12qaz@yandex.ru>
2022-02-10 17:51:10 +0300

    fv

--- db/models.py
+++ db/models.py
@@ -3,7 +3,7 @@ from tortoise import fields
 from flask_security import UserMixin, RoleMixin
 
 
-class telegramuser(Model):
+class TelegramUser(Model):
     id = fields.IntField(pk=True)
     telegram_id = fields.BigIntField(unique=True, index=True)
     username = fields.CharField(128, unique=True)
