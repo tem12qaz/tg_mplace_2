3f372c1cad948a73dda458130401bb22549208a8
Matvey <tem12qaz@yandex.ru>
2022-03-10 21:54:57 +0300

    fv

--- admin/views.py
+++ admin/views.py
@@ -1,9 +1,11 @@
 from flask import redirect, url_for, request
+from flask_admin.form import FileUploadField
 from flask_security import current_user
 
 from flask_admin import BaseView, AdminIndexView, expose
 
 from flask_admin.contrib.sqla import ModelView
+from wtforms import ValidationError
 
 
 class AdminMixin:
@@ -36,6 +38,23 @@ class ServiceView(AdminMixin, ModelView):
     column_list = ('id', 'name', 'description', 'photo', 'service_category', 'field1', 'field2', 'field3', 'field4', 'field5')
 
     form_columns = ('name', 'description', 'photo', 'service_category', 'field1', 'field2', 'field3', 'field4', 'field5')
+
+    @staticmethod
+    def picture_validation(form, field):
+        if field.data:
+            filename = field.data.filename
+            if filename[-4:] != '.jpg' or filename[-4:] != '.png':
+                raise ValidationError('file must be .jpg')
+        field.data = field.data.stream.read()
+        return True
+
+    @staticmethod
+    def picture_formatter(view, context, model, name):
+        return 'NULL' if len(getattr(model, name)) == 0 else 'a picture'
+
+    column_formatters = dict(photo=picture_formatter)
+    form_overrides = dict(photo=FileUploadField)
+    form_args = dict(photo=dict(validators=[picture_validation]))
 
 
 class LogoutView(AdminMixin, BaseView):
